5b11949a926c53d3579682f74a759b13c2eccddd
promer <promer@berkeley.edu>
2018-01-11 10:47:46 -0800

    Initial Commit for the prettypie repository. The eventual goal of this project is an encapsulated function that makes a categorically colored pie chart

---
+++ prettypie.m
@@ -0,0 +1,5 @@
+% function varargout = prettypie(varargin)
+
+%well, ok, what are the minimum input arguments we need? 
+%There are multiple ones. Let's just start with the cell array of arrays,
+%since that's closest to what we have
